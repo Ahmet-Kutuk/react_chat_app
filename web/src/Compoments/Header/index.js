ec1136988827b8d0b8b2a42032bb955cb4a9bd1a
Ahmet Kutuk <ahmetcankutk@gmail.com>
2021-06-11 20:20:16 +0300

    header compoment created

---
+++ web/src/Compoments/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {HeaderDıv} from '../../styles/Header';
+function Header(props) {
+    return (
+        <>
+            <HeaderDıv>
+                <input onChange={(e) => props.action(e)} placeholder="Your Name" value={props.name} name="name" type="text" />
+            </HeaderDıv>
+        </>
+    )
+}
+
+export default Header
---
+++ web/src/styles/Header.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const HeaderDıv = styled.div`
+background: rgba(0, 0, 0, 0.5);
+width:600px;
+padding: 10px 10px 10px 50px;
+border-radius:15px 15px 0px 0px;
+
+
+`;
